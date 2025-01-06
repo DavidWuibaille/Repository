6825c20d8b700011fd08adf747991373e79149e7
David Wuibaille <david.wuibaille@outlook.fr>
2025-01-06 13:15:04 +0100

    Create DashboardEPM.ps1

---
+++ Function/DashboardEPM.ps1
@@ -0,0 +1,8 @@
+function Get-SqlData($connection, $query) {
+    $command = $connection.CreateCommand()
+    $command.CommandText = $query
+    $result = $command.ExecuteReader()
+    $table = New-Object System.Data.DataTable
+    $table.Load($result)
+    return $table
+}
