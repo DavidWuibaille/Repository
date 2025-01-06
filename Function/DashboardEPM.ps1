e1a056cc4983304391434a5ab3bcd816a6b18c34
David Wuibaille <david.wuibaille@outlook.fr>
2025-01-06 13:55:31 +0100

    Update DashboardEPM.ps1

--- Function/DashboardEPM.ps1
+++ Function/DashboardEPM.ps1
@@ -80,3 +80,89 @@ function Get-ApplicationData {
     # Retourner la collection
     return $ApplicationData
 }
+
+
+function Get-BitlockerDetails {
+    param (
+        [Parameter(Mandatory = $true)]
+        [System.Data.SqlClient.SqlConnection]$Connection,
+
+        [Parameter(Mandatory = $true)]
+        [string]$OSTypeFilter = "Windows 10"  # Filtre par défaut pour le système d'exploitation
+    )
+
+    # Initialiser la collection pour stocker les données
+    $BitlockerDetails = @()
+
+    # Définir la requête SQL avec le filtre dynamique pour le système d'exploitation
+    $query = @"
+        SELECT DISTINCT A0.DISPLAYNAME, A2.SECUREBOOTENABLED, A2.UEFIENABLED, A3.CONVERSIONSTATUS,
+                        A4.TPMENABLE, A4.TPMVERSION, A5.MODEL
+        FROM Computer A0 (nolock)
+        LEFT OUTER JOIN Operating_System A1 (nolock) ON A0.Computer_Idn = A1.Computer_Idn
+        LEFT OUTER JOIN BIOS A2 (nolock) ON A0.Computer_Idn = A2.Computer_Idn
+        LEFT OUTER JOIN BitLocker A3 (nolock) ON A0.Computer_Idn = A3.Computer_Idn
+        LEFT OUTER JOIN TPMSystem A4 (nolock) ON A0.Computer_Idn = A4.Computer_Idn
+        LEFT OUTER JOIN CompSystem A5 (nolock) ON A0.Computer_Idn = A5.Computer_Idn
+        WHERE (A1.OSTYPE LIKE N'%$OSTypeFilter%')
+        ORDER BY A0.DISPLAYNAME
+"@
+
+    # Exécuter la requête et charger les résultats
+    $table = Get-SqlData -Connection $Connection -Query $query
+
+    # Parcourir les résultats et remplir la collection
+    foreach ($element in $table) {
+        $BitlockerDetails += [PSCustomObject]@{
+            'DEVICENAME'    = $element.DISPLAYNAME
+            'SECURE Boot'   = $element.SECUREBOOTENABLED
+            'UEFI'          = $element.UEFIENABLED
+            'BitLocker'     = $element.CONVERSIONSTATUS
+            'TPM'           = $element.TPMENABLE
+            'TPM Version'   = $element.TPMVERSION
+            'Model'         = $element.MODEL
+        }
+    }
+
+    # Retourner la collection d'objets
+    return $BitlockerDetails
+}
+
+
+function Get-WindowsDetails {
+    param (
+        [Parameter(Mandatory = $true)]
+        [System.Data.SqlClient.SqlConnection]$Connection,
+
+        [Parameter(Mandatory = $true)]
+        [string]$OSTypeFilter = "Windows 1"  # Filtre par défaut pour le système d'exploitation
+    )
+
+    # Initialiser la collection pour stocker les données
+    $WindowsDetails = @()
+
+    # Définir la requête SQL avec le filtre dynamique pour le système d'exploitation
+    $query = @"
+        SELECT DISTINCT A0.DISPLAYNAME, A1.OSTYPE, A2.CURRENTBUILD, A2.UBR
+        FROM Computer A0 (nolock)
+        LEFT OUTER JOIN Operating_System A1 (nolock) ON A0.Computer_Idn = A1.Computer_Idn
+        LEFT OUTER JOIN OSNT A2 (nolock) ON A0.Computer_Idn = A2.Computer_Idn
+        WHERE (A1.OSTYPE LIKE N'%$OSTypeFilter%')
+        ORDER BY A0.DISPLAYNAME
+"@
+
+    # Exécuter la requête et charger les résultats
+    $table = Get-SqlData -Connection $Connection -Query $query
+
+    # Parcourir les résultats et remplir la collection
+    foreach ($element in $table) {
+        $versionFull  = "$($element.CURRENTBUILD).$($element.UBR)"  # Version complète
+        $WindowsDetails += [PSCustomObject]@{
+            'DEVICENAME' = $element.DISPLAYNAME
+            'VERSION'    = $versionFull
+        }
+    }
+
+    # Retourner la collection d'objets
+    return $WindowsDetails
+}
