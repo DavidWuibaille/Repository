d3dbdfbfef8148a2f0a7a52490b4c8c4bcffbcf7
David Wuibaille <david.wuibaille@outlook.fr>
2025-03-10 12:06:25 +0100

    Update logonscript.ps1

--- lab/logonscript.ps1
+++ lab/logonscript.ps1
@@ -1,5 +1,7 @@
 # Désactive le démarrage du service "Tanium Client"
 Set-Service -Name "Tanium Client" -StartupType Disabled
+Set-Service -Name "TaniumDriverSvc" -StartupType Disabled
 
 # Arrête le service "Tanium Client"
 Stop-Service -Name "Tanium Client" -Force
+Stop-Service -Name "TaniumDriverSvc" -Force
